d58f608c87a157f0245d3845cdb30202778e164d
DevOps Renovate <service_account_5e4f69f14c19e62f6c8e02ac98326d1c@noreply.gitlab.protontech.ch>
2024-10-17 17:44:51 +0000

    chore(deps): update dependency mxcl/version to v2.1.0

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.2.3"),
         .package(url: "https://github.com/jpsim/Yams", exact: "5.0.6"),
         .package(url: "https://github.com/protonjohn/SwiftGit2", exact: "0.12.0"),
-        .package(url: "https://github.com/mxcl/Version", exact: "2.0.1"),
+        .package(url: "https://github.com/mxcl/Version", exact: "2.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-parsing", exact: "0.13.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", exact: "1.0.0"),
         .package(url: "https://github.com/almazrafi/DictionaryCoder", exact: "1.1.0"),
